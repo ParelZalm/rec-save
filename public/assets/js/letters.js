d3fb113a89d98673773a784b27cb9a1cdf6a4607
ParelZalm <thefinestintern@gmail.com>
2022-05-16 16:52:28 +0200

    remove unwanted + add dev button

--- public/assets/js/letters.js
+++ public/assets/js/letters.js
@@ -1,35 +1,9 @@
+const myText = document.querySelector(".letters");
+const animationBlock = document.querySelector('.part-1');
+const recordBlock = document.querySelector(".part-6");
 
-// let textanimation = anime.timeline({
-//     loop: false,
-// });
-//     textanimation
-//   .add({
-//     targets: '.ml11 .line',
-//     scaleY: [0,1],
-//     opacity: [0.5,1],
-//     easing: "easeOutExpo",
-//     duration: 700
-//   })
-//   .add({
-//     targets: '.ml11 .line',
-//     translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
-//     easing: "easeOutExpo",
-//     duration: 700,
-//     delay: 100
-//   }).add({
-//     targets: '.ml11 .letter',
-//     opacity: [0,1],
-//     easing: "easeOutExpo",
-//     duration: 600,
-//     offset: '-=775',
-//     delay: (el, i) => 34 * (i+1)
-//   }).add({
-//     targets: '.ml11',
-//     opacity: 1,
-//     duration: 1000,
-//     easing: "easeOutExpo",
-//     delay: 1000
-//   });
+// intiger to loop over myNewText state
+let  currentText = 0;
 
 // declare text for nextText()
 const myNewText = [
@@ -37,12 +11,6 @@ const myNewText = [
     "This a good way to mourn and it also help others by sharing",
     "example here",
     ]
-// intiger to loop over myNewText state
-let  currentText = 0;
-
-const myText = document.querySelector(".letters");
-const animationBlock = document.querySelector('.part-1');
-const recordBlock = document.querySelector(".part-6");
 
 // timeout function
 function resolveAftersec() {
@@ -61,7 +29,7 @@ async function nextText(){
         console.log("the - end")
         animationBlock.style.display = "none";
         recordBlock.style.display = "block";
-        const result = await resolveAftersec();
+        await resolveAftersec();
         // recordBlock.classlist.add("animation-class");
         return;
     }
@@ -77,5 +45,11 @@ async function nextText(){
     // add to intiger counter
     currentText++;
 
+  }
+
+  function endText(){
+    currentText += 3;
+    nextText();
+    console.log(currentText);
   }
   
